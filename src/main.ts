f67f165d445514664c5bc2270725dfe787afef5f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-06 06:06:30 +0200

    Add drawing and switching between modes

--- src/main.ts
+++ src/main.ts
@@ -14,34 +14,36 @@ if (ctx == null) {
     throw Error("Context 2D is not supported");
 }
 
-array[0][0] = 1;
-canvas.width =  CANVAS_WIDTH;
+enum MotionMode {
+    Insert,
+    Normal
+}
+
+const cursorPosition: [number, number, MotionMode] = [0, 0, MotionMode.Normal];
+
+canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
-const colorTable = ["#a5a5a5", "blue", "red"];
+const colorTable = ["#a5a5a5", "blue"];
 setInterval(() => drawTable(ctx), 5);
-addEventListener("keypress", (e) => {
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array[i].length; j++) {
-            if (array[i][j] != 1) {
-                continue;
-            }
+addEventListener("keydown", (e) => {
+    console.log(e.key);
+    if (e.key == "i") {
+        cursorPosition[2] = MotionMode.Insert;
+    } else if (e.key == "Escape") {
+        cursorPosition[2] = MotionMode.Normal;
+    } else if (e.key == "l" && cursorPosition[1] < TILE_SIZE - 1) {
+        cursorPosition[1]++;
+    } else if (e.key == "h" && cursorPosition[1] > 0) {
+        cursorPosition[1]--;
+    } else if (e.key == "j" && cursorPosition[0] < TILE_SIZE - 1) {
+        cursorPosition[0]++;
+    } else if (e.key == "k" && cursorPosition[0] > 0) {
+        cursorPosition[0]--;
+    }
 
-            if (e.key == "l" && j < TILE_SIZE - 1) {
-                array[i][j] = 0;
-                array[i][j + 1] = 1;
-            } else if (e.key == "h" && j > 0){
-                array[i][j] = 0;
-                array[i][j - 1] = 1;
-            } else if (e.key == "j" && i < TILE_SIZE - 1) {
-                array[i][j] = 0;
-                array[i + 1][j] = 1;
-            } else if (e.key == "k" && i > 0) {
-                array[i][j] = 0;
-                array[i - 1][j] = 1;
-            }
-            return;
-        }
+    if (cursorPosition[2] == MotionMode.Insert) {
+        array[cursorPosition[0]][cursorPosition[1]] = 1;
     }
 });
 
@@ -50,9 +52,14 @@ function drawTable(ctx: CanvasRenderingContext2D) {
         for (let j = 0; j < array[i].length; j++) {
             let lineHeight = Math.floor(i * TILE_SIZE);
             let lineWidth = Math.floor(j * TILE_SIZE);
-            ctx.strokeRect(lineWidth, lineHeight, TILE_SIZE , TILE_SIZE);
+            ctx.strokeRect(lineWidth, lineHeight, TILE_SIZE, TILE_SIZE);
             ctx.fillStyle = colorTable[array[i][j]];
-            ctx.fillRect(lineWidth, lineHeight, TILE_SIZE , TILE_SIZE);
+            ctx.fillRect(lineWidth, lineHeight, TILE_SIZE, TILE_SIZE);
         }
     }
+
+    let lineHeight = Math.floor(cursorPosition[0] * TILE_SIZE);
+    let lineWidth = Math.floor(cursorPosition[1] * TILE_SIZE);
+    ctx.fillStyle = cursorPosition[2] == MotionMode.Normal ? "black" : "red";
+    ctx.fillRect(lineWidth, lineHeight, TILE_SIZE, TILE_SIZE);
 }
