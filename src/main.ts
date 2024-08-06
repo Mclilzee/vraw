27e81b9535d39ecba5e6ef9f992373687dec5433
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-06 05:48:45 +0200

    Add moving the cursor constantly

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './style.css'
 const TILE_SIZE = 20;
 const CANVAS_WIDTH = 400;
 const CANVAS_HEIGHT = 400;
+const array = Array(TILE_SIZE).fill(0).map(() => Array(TILE_SIZE).fill(0));
+
 const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
 if (canvas == null) {
     throw Error("Canvas were not found");
@@ -12,26 +14,29 @@ if (ctx == null) {
     throw Error("Context 2D is not supported");
 }
 
-let array = Array(TILE_SIZE).fill(0).map(() => Array(TILE_SIZE).fill(0));
 array[0][0] = 1;
 canvas.width =  CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
 const colorTable = ["#a5a5a5", "blue", "red"];
-drawTable(ctx);
-
+setInterval(() => drawTable(ctx), 5);
 addEventListener("keypress", () => {
-    array[0][0] = 0;
-    array[1][1] = 1;
-    drawTable(ctx);
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j] == 1 && j < TILE_SIZE - 1) {
+                array[i][j] = 0;
+                array[i][j + 1] = 1;
+               return;
+            }
+        }
+    }
 });
-
 
 function drawTable(ctx: CanvasRenderingContext2D) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array[i].length; j++) {
-            let lineWidth = Math.floor(i * TILE_SIZE);
-            let lineHeight = Math.floor(j * TILE_SIZE);
+            let lineHeight = Math.floor(i * TILE_SIZE);
+            let lineWidth = Math.floor(j * TILE_SIZE);
             ctx.strokeRect(lineWidth, lineHeight, TILE_SIZE , TILE_SIZE);
             ctx.fillStyle = colorTable[array[i][j]];
             ctx.fillRect(lineWidth, lineHeight, TILE_SIZE , TILE_SIZE);
