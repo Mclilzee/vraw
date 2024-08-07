ebaa4b9892ca7031c366f3c1075f563faa8fe7bf
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-07 05:47:13 +0200

    Add switching to normal mode

--- src/input.ts
+++ src/input.ts
@@ -1,6 +1,12 @@
 import { Cursor } from "./cursor";
 const CELL_DEFAULT_COLOR = "#a5a5a5";
 
+enum Operation {
+    Delete,
+    Draw,
+    Move,
+}
+
 export class DrawingBoard {
     cursor = new Cursor();
     width: number;
@@ -27,67 +33,104 @@ export class DrawingBoard {
     }
 
     moveCursorRight() {
-        let newPos = this.cursor.y + this.moves;
+        let newPos = this.cursor.y + (this.moves == 0 ? 1 : this.moves);
         if (newPos > this.width - 1) {
             newPos = this.width - 1;
         }
+
+        if (this.cursor.inInsertMode()) {
+            this.drawHorizontalLine(this.cursor.y, newPos);
+        } else if (this.cursor.inDeleteMode()) {
+            this.deleteHorizontalLine(this.cursor.y, newPos);
+            this.cursor.switchToNormal();
+        }
+
         this.cursor.y = newPos;
-        this.moves = 1;
+        this.moves = 0;
     }
 
     moveCursorLeft() {
-        let newPos = this.cursor.y - this.moves;
+        let newPos = this.cursor.y - (this.moves == 0 ? 1 : this.moves);
         if (newPos < 0) {
             newPos = 0;
         }
+
+        if (this.cursor.inInsertMode()) {
+            this.drawHorizontalLine(newPos, this.cursor.y);
+        } else if (this.cursor.inDeleteMode()) {
+            this.deleteHorizontalLine(newPos, this.cursor.y);
+            this.cursor.switchToNormal();
+        }
+
         this.cursor.y = newPos;
-        this.moves = 1;
+        this.moves = 0;
     }
 
     moveCursorUp() {
-        let newPos = this.cursor.x - this.moves;
+        let newPos = this.cursor.x - (this.moves == 0 ? 1 : this.moves);
         if (newPos < 0) {
             newPos = 0;
         }
+
+        if (this.cursor.inInsertMode()) {
+            this.drawVerticalLine(newPos, this.cursor.x);
+        } else if (this.cursor.inDeleteMode()) {
+            this.deleteHorizontalLine(newPos, this.cursor.x);
+            this.cursor.switchToNormal();
+        }
+
         this.cursor.x = newPos;
-        this.moves = 1;
+        this.moves = 0;
     }
 
     moveCursorDown() {
-        let newPos = this.cursor.x + this.moves;
+        let newPos = this.cursor.x + (this.moves == 0 ? 1 : this.moves);
         if (newPos > this.height - 1) {
             newPos = this.height - 1;
         }
 
         if (this.cursor.inInsertMode()) {
-            this.drawVerticalLine(this.cursor.x, newPos, this.cursor.y, this.drawingColor);
+            this.drawVerticalLine(this.cursor.x, newPos);
         } else if (this.cursor.inDeleteMode()) {
-            this.drawVerticalLine(this.cursor.x, newPos, this.cursor.y, CELL_DEFAULT_COLOR);
+            this.deleteHorizontalLine(this.cursor.x, newPos);
+            this.cursor.switchToNormal();
         }
 
         this.cursor.x = newPos;
-        this.moves = 1;
+        this.moves = 0;
     }
 
-    drawVerticalLine(start: number, end: number, y: number, color: string) {
+    drawVerticalLine(start: number, end: number) {
         for(let i = start; i <= end; i++){
-            this.board[i][y] = color;
+            this.board[i][this.cursor.y] = this.drawingColor;
         }
     }
 
-    drawHorizontalLine(start: number, end: number, x: number, color: string) {
+    drawHorizontalLine(start: number, end: number) {
         for(let i = start; i <= end; i++){
-            this.board[x][i] = color;
+            this.board[this.cursor.x][i] = this.drawingColor;
+        }
+    }
+
+    deleteVerticalLine(start: number, end: number) {
+        for(let i = start; i <= end; i++){
+            this.board[i][this.cursor.y] = CELL_DEFAULT_COLOR;
+        }
+    }
+
+    deleteHorizontalLine(start: number, end: number) {
+        for(let i = start; i <= end; i++){
+            this.board[this.cursor.x][i] = CELL_DEFAULT_COLOR;
         }
     }
 
     handleInput(input: string) {
-        if (input == "0" && this.moves == 1) {
+        if (input == "0" && this.moves == 0) {
             this.moveToRowStart();
         } else {
             const num = parseInt(input);
             if (!isNaN(num)) {
-                this.moves = this.moves == 1 ? num : this.moves * 10 + num;
+                this.moves = this.moves == 0 ? num : this.moves * 10 + num;
             }
         }
 
