6e3bbbffcec6844348aa6013d429236809078f0f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-13 10:52:32 +0200

    Add relative numbers

--- src/main.ts
+++ src/main.ts
@@ -43,29 +43,38 @@ function draw(board: DrawingBoard) {
 
   ctx.fillStyle = board.cursorColor();
   ctx.fillRect(board.cursor.y * width + V_NUMBER_PADDING, board.cursor.x * height, width, height);
-  drawVerticalRelativeNumbers(board);
-  drawHorizontalRelativeNumbers(board);
+  drawNumbers(board);
 }
+getNumbers(20, TILE_SIZE);
 
-function drawVerticalRelativeNumbers(board: DrawingBoard) {
+function drawNumbers(board: DrawingBoard) {
+  const verticalNumbers = getNumbers(board.cursor.x, board.height);
   const height = CANVAS_HEIGHT / board.height;
-  for (let i = 0; i < board.height; i++) {
+  const width = CANVAS_WIDTH / board.width;
+
+  for (let i = 0; i < verticalNumbers.length; i++) {
     const y = i * height + (V_NUMBER_PADDING / 2) - 3;
     ctx.fillStyle = "white";
     ctx.font = "12px Fira Sans";
     ctx.textAlign = "center";
-    ctx.fillText(i.toString(), 0 + V_NUMBER_PADDING / 2, y);
+    ctx.fillText(verticalNumbers[i].toString(), 0 + V_NUMBER_PADDING / 2, y);
   }
-}
 
-function drawHorizontalRelativeNumbers(board: DrawingBoard) {
-  const width = CANVAS_WIDTH / board.width;
-  for (let i = 0; i < board.width; i++) {
+  const horizontalNumbers = getNumbers(board.cursor.y, board.width);
+  for (let i = 0; i < horizontalNumbers.length; i++) {
     const x = i * width + V_NUMBER_PADDING + V_NUMBER_PADDING / 4;
     ctx.fillStyle = "white";
     ctx.font = "12px Fira Sans";
     ctx.textAlign = "center";
-    ctx.fillText(i.toString(), x, CANVAS_WIDTH + V_NUMBER_PADDING / 2);
+    ctx.fillText(horizontalNumbers[i].toString(), x, CANVAS_WIDTH + V_NUMBER_PADDING / 2);
   }
 }
 
+function getNumbers(anchor: number, size: number) {
+  const array = [];
+  for (let i = anchor; i > anchor - size; i--) {
+    array.push(Math.abs(i));
+  }
+
+  return array;
+}
