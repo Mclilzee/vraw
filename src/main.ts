ab36b5ba2eed22fa574b06f704bac42817daab1c
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-13 10:29:27 +0200

    Fix numbers padding to be correctly set

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { DrawingBoard } from './drawingBoard';
 const TILE_SIZE = 40;
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
-const RELATIVE_NUMBER_PADDING = 10;
+const RELATIVE_NUMBER_PADDING = 30;
 
 const drawingBoard = new DrawingBoard(TILE_SIZE, TILE_SIZE);
-canvas.width = CANVAS_WIDTH;
+canvas.width = CANVAS_WIDTH + RELATIVE_NUMBER_PADDING;
 canvas.height = CANVAS_HEIGHT;
 
 draw(drawingBoard);
@@ -27,20 +27,22 @@ addEventListener("keyup", (e) => {
 });
 
 function draw(board: DrawingBoard) {
-  const width = canvas.width / board.width;
+  ctx.reset();
+  const width = CANVAS_WIDTH / board.width;
   const height = canvas.height / board.height;
   for (let i = 0; i < board.width; i++) {
     for (let j = 0; j < board.height; j++) {
-      ctx.strokeRect(j * width, i * height, width, height);
+      const x = j * width + RELATIVE_NUMBER_PADDING;
+      ctx.strokeRect(x, i * height, width + RELATIVE_NUMBER_PADDING, height);
       ctx.fillStyle = board.board[i][j];
-      ctx.fillRect(j * width, i * height, width, height);
+      ctx.fillRect(x, i * height, width, height);
       ctx.fillStyle = board.visualMask[i][j];
-      ctx.fillRect(j * width, i * height, width, height);
+      ctx.fillRect(x, i * height, width, height);
     }
   }
 
   ctx.fillStyle = board.cursorColor();
-  ctx.fillRect(board.cursor.y * width, board.cursor.x * height, width, height);
+  ctx.fillRect(board.cursor.y * width + RELATIVE_NUMBER_PADDING, board.cursor.x * height, width, height);
   drawVerticalRelativeNumbers(board);
 }
 
@@ -48,9 +50,9 @@ function drawVerticalRelativeNumbers(board: DrawingBoard) {
   const height = canvas.height / board.height;
   for (let i = 0; i <= board.height; i++) {
     ctx.fillStyle = "white";
-    ctx.font = "15px Fira Sans";
+    ctx.font = "14px Fira Sans";
     ctx.textAlign = "center";
-    ctx.fillText("1", RELATIVE_NUMBER_PADDING / 2, (i * height) - height / 4);
+    ctx.fillText(i.toString(), RELATIVE_NUMBER_PADDING / 2, (i * height) - height / 4);
   }
 }
 
