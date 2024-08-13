ca674f37490656b952b3ee56939fda8f240f1281
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-13 10:05:24 +0200

    Add drawing relative numbers to the side

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DrawingBoard } from './drawingBoard';
 const TILE_SIZE = 40;
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
+const RELATIVE_NUMBER_PADDING = 10;
 
 const drawingBoard = new DrawingBoard(TILE_SIZE, TILE_SIZE);
 canvas.width = CANVAS_WIDTH;
@@ -40,4 +41,16 @@ function draw(board: DrawingBoard) {
 
   ctx.fillStyle = board.cursorColor();
   ctx.fillRect(board.cursor.y * width, board.cursor.x * height, width, height);
+  drawVerticalRelativeNumbers(board);
 }
+
+function drawVerticalRelativeNumbers(board: DrawingBoard) {
+  const height = canvas.height / board.height;
+  for (let i = 0; i <= board.height; i++) {
+    ctx.fillStyle = "white";
+    ctx.font = "15px Fira Sans";
+    ctx.textAlign = "center";
+    ctx.fillText("1", RELATIVE_NUMBER_PADDING / 2, (i * height) - height / 4);
+  }
+}
+
