a2a563950e3c89d6bdd80d4a2d5443c874c9c80c
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-18 20:50:55 +0200

    Add previous key display

--- src/render.ts
+++ src/render.ts
@@ -3,14 +3,14 @@ import { boardCanvas, boardCtx, statusInfo, statusInfoCtx } from './elements';
 
 const BOARD_WIDTH = 600;
 const BOARD_HEIGHT = 600;
-const NUMBER_PADDING = 30;
+const RIGHT_TEXT_PADDING = 30;
 const BAR_HEIGHT = 15;
 const CURSOR_POSITION_RIGHT_PADDING = 15;
 const TEXT_PADDING = 3;
 const STATUS_BAR_INFO_HEIGHT = 20;
 
-boardCanvas.width = BOARD_WIDTH + NUMBER_PADDING * 2;
-boardCanvas.height = BOARD_HEIGHT + NUMBER_PADDING + BAR_HEIGHT;
+boardCanvas.width = BOARD_WIDTH + RIGHT_TEXT_PADDING * 2;
+boardCanvas.height = BOARD_HEIGHT + RIGHT_TEXT_PADDING + BAR_HEIGHT;
 statusInfo.width = boardCanvas.width;
 statusInfo.height = STATUS_BAR_INFO_HEIGHT;
 
@@ -21,7 +21,7 @@ export function renderBoard(board: Board) {
   const height = BOARD_HEIGHT / board.rows;
   for (let i = 0; i < board.rows; i++) {
     for (let j = 0; j < board.columns; j++) {
-      const x = j * width + NUMBER_PADDING;
+      const x = j * width + RIGHT_TEXT_PADDING;
       boardCtx.strokeRect(x, i * height, width, height);
       boardCtx.fillStyle = currentBoard[i][j];
       boardCtx.fillRect(x, i * height, width, height);
@@ -31,16 +31,20 @@ export function renderBoard(board: Board) {
   }
 
   boardCtx.fillStyle = board.cursorColor();
-  boardCtx.fillRect(board.cursor.y * width + NUMBER_PADDING, board.cursor.x * height, width, height);
+  boardCtx.fillRect(board.cursor.y * width + RIGHT_TEXT_PADDING, board.cursor.x * height, width, height);
   renderBoardNumbers(board);
   renderBoardStatusBar(board.cursor.x + 1, board.cursor.y + 1);
 }
 
-export function renderStatusInfo(info: string, color: string) {
+export function renderStatusInfo(info: string, color: string, previousKey?: string) {
   statusInfoCtx.reset();
   statusInfoCtx.fillStyle = color;
   statusInfoCtx.font = "15px Fira Sans";
   statusInfoCtx.fillText(info, TEXT_PADDING, STATUS_BAR_INFO_HEIGHT);
+  if (previousKey != undefined) {
+    statusInfoCtx.fillStyle = "black";
+    statusInfoCtx.fillText(previousKey,  statusInfo.width - RIGHT_TEXT_PADDING, STATUS_BAR_INFO_HEIGHT);
+  }
 }
 
 function renderBoardNumbers(board: Board) {
@@ -49,7 +53,7 @@ function renderBoardNumbers(board: Board) {
   const width = BOARD_WIDTH / board.columns;
 
   for (let i = 0; i < verticalNumbers.length; i++) {
-    const y = i * height + (NUMBER_PADDING / 2) - TEXT_PADDING;
+    const y = i * height + (RIGHT_TEXT_PADDING / 2) - TEXT_PADDING;
     boardCtx.fillStyle = "white";
     boardCtx.font = "12px Fira Sans";
     boardCtx.textAlign = "center";
@@ -57,12 +61,12 @@ function renderBoardNumbers(board: Board) {
     if (number === 0) {
       boardCtx.fillStyle = "red";
     }
-    boardCtx.fillText(number.toString(), NUMBER_PADDING / 2, y);
+    boardCtx.fillText(number.toString(), RIGHT_TEXT_PADDING / 2, y);
   }
 
   const horizontalNumbers = getNumbers(board.cursor.y, board.columns);
   for (let i = 0; i < horizontalNumbers.length; i++) {
-    const x = i * width + NUMBER_PADDING + NUMBER_PADDING / 4;
+    const x = i * width + RIGHT_TEXT_PADDING + RIGHT_TEXT_PADDING / 4;
     boardCtx.fillStyle = "white";
     boardCtx.font = "10px Fira Sans";
     boardCtx.textAlign = "center";
@@ -70,7 +74,7 @@ function renderBoardNumbers(board: Board) {
     if (number === 0) {
       boardCtx.fillStyle = "red";
     }
-    boardCtx.fillText(number.toString(), x, BOARD_WIDTH + NUMBER_PADDING / 2);
+    boardCtx.fillText(number.toString(), x, BOARD_WIDTH + RIGHT_TEXT_PADDING / 2);
   }
 }
 
@@ -84,7 +88,7 @@ function getNumbers(anchor: number, size: number) {
 }
 
 function renderBoardStatusBar(cursorRow: number, cursorColumn: number) {
-  const y = BOARD_WIDTH + NUMBER_PADDING;
+  const y = BOARD_WIDTH + RIGHT_TEXT_PADDING;
   boardCtx.fillStyle = "grey";
   boardCtx.fillRect(0, y, boardCanvas.width, BAR_HEIGHT);
   boardCtx.fillStyle = "white";
