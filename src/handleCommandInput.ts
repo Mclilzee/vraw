d0ff55cddb76892fd6a3399bf324fda7cabd06ce
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-09-18 00:33:31 +0200

    Refactor command prompt

--- src/handleCommandInput.ts
+++ src/handleCommandInput.ts
@@ -1,8 +1,14 @@
 import { renderStatusInfo } from "./render";
 
-let command = "";
+let command = ":";
 
 export default function handleCommandInput(e: KeyboardEvent) {
-    command += e.key;
+    if (e.key === "Backspace") {
+        command = command.substring(0, command.length - 1);
+    } else if (e.key === ":" && command.length >= 1) {
+        command = command + e.key;
+    } else {
+        command += e.key;
+    }
   renderStatusInfo(command, "green");
 }
