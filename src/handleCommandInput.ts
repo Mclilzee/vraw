ef112880973577ce2c462f18a18b3e10cf6ca0dc
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-09-18 00:40:00 +0200

    Fix the info drawing

--- src/handleCommandInput.ts
+++ src/handleCommandInput.ts
@@ -1,11 +1,16 @@
 import { renderStatusInfo } from "./render";
 
 let command = ":";
+const uselessKeyEvents = ["Shift", "Control"]
 
 export default function handleCommandInput(e: KeyboardEvent) {
+    if (uselessKeyEvents.includes(e.key)) {
+        return;
+    }
+
     if (e.key === "Backspace") {
-        command = command.substring(0, command.length - 1);
-    } else if (e.key === ":" && command.length >= 1) {
+        command = command.substring(0, Math.max(1, command.length - 1));
+    } else if (e.key === ":" && command.length > 1) {
         command = command + e.key;
     } else {
         command += e.key;
