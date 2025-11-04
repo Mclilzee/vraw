034f86781861d951439f98c59e08e40c05585a18
Emad Ali <70924991+Mclilzee@users.noreply.github.com>
2025-11-05 00:20:33 +0100

    Draw bottom numbers

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
+#include <cmath>
 #include <iostream>
 #include <raylib.h>
+#include <string>
 #include <vector>
 
-static const int SCREEN_WIDTH = 800;
-static const int SCREEN_HEIGHT = 800;
+static const int SCREEN_WIDTH = 1000;
+static const int SCREEN_HEIGHT = 1000;
+static const int WIDTH = 800;
+static const int HEIGHT = 800;
 static const int CX = SCREEN_WIDTH / 2;
 static const int CY = SCREEN_HEIGHT / 2;
-static const int WIDTH = 600;
-static const int HEIGHT = 600;
-static const int NUMBERS_LEFT_BAR_PADDING = 30;
+static const int NUMBERS_LEFT_BAR_PADDING = 20;
 static const int BAR_HEIGHT = 15;
 static const int CURSOR_POSITION_RIGHT_PADDING = 15;
 static const int TEXT_PADDING = 3;
 static const int VERTICAL_TEXT_PADDING = 5;
-static const int NUMBERS_BOTTOM_BAR_PADDING = 25;
-static const int CANVAS_WIDTH = WIDTH + NUMBERS_LEFT_BAR_PADDING * 2;
-static const int CANVAS_HEIGHT = HEIGHT + NUMBERS_LEFT_BAR_PADDING + BAR_HEIGHT;
-static const int STATUS_INFO_WIDTH = CANVAS_WIDTH;
-static const int STATUS_INFO_HEIGHT = NUMBERS_BOTTOM_BAR_PADDING;
+static const int NUMBERS_BOTTOM_BAR_PADDING = 10;
 static const Vector2 BOARD_START{CX - WIDTH / 2,
                                  CY - HEIGHT / 2 - NUMBERS_BOTTOM_BAR_PADDING};
 static const Vector2 BOARD_END{CX + WIDTH / 2,
@@ -54,43 +52,88 @@ class Board {
     }
 };
 
-void render_board(Board *board) {
+std::vector<int> get_numbers(int anchor, int size) {
+    std::vector<int> array;
+    for (int i = anchor; i > anchor - size; i--) {
+        array.push_back(std::abs(i));
+    }
+
+    return array;
+}
+
+void render_board_number(Board board) {
+    std::vector<int> vertical_numbers = get_numbers(board.cursor.x, board.rows);
+    int height = HEIGHT / board.rows;
+    int width = WIDTH / board.columns;
+    int font_size = height / 2;
+    int font_padding = font_size / 2;
+
+    for (int i = 0; i < vertical_numbers.size(); i++) {
+        int y = BOARD_START.y + i * height + font_padding;
+        int number = vertical_numbers.at(i);
+
+        if (number == 0) {
+            DrawText(std::to_string(number).c_str(),
+                     BOARD_START.x - NUMBERS_LEFT_BAR_PADDING, y, font_size,
+                     RED);
+        } else {
+            DrawText(std::to_string(number).c_str(),
+                     BOARD_START.x - NUMBERS_LEFT_BAR_PADDING, y, font_size,
+                     WHITE);
+        }
+
+        std::vector<int> horizontal_numbers =
+            get_numbers(board.cursor.y, board.columns);
+        int x = BOARD_START.x + i * width + font_padding;
+        number = horizontal_numbers.at(i);
+        if (number == 0) {
+            DrawText(std::to_string(number).c_str(), x,
+                     BOARD_END.y + NUMBERS_BOTTOM_BAR_PADDING, font_size, RED);
+        } else {
+            DrawText(std::to_string(number).c_str(), x,
+                     BOARD_END.y + NUMBERS_BOTTOM_BAR_PADDING, font_size,
+                     WHITE);
+        }
+    }
+}
+
+void render_board(Board board) {
     ClearBackground(DARKGRAY);
-    int width = WIDTH / board->columns;
-    int height = HEIGHT / board->rows;
-    for (int i = 0; i < board->rows; i++) {
-        for (int j = 0; j < board->rows; j++) {
+    int width = WIDTH / board.columns;
+    int height = HEIGHT / board.rows;
+    for (int i = 0; i < board.rows; i++) {
+        for (int j = 0; j < board.rows; j++) {
             int x = BOARD_START.x + j * width;
             int y = BOARD_START.y + i * height;
             DrawRectangle(x, y, width, height,
-                          board->cells.at(board->rows * i + j));
+                          board.cells.at(board.rows * i + j));
             DrawRectangle(x, y, width, height,
-                          board->visual_mask.at(board->rows * i + j));
+                          board.visual_mask.at(board.rows * i + j));
         }
     }
 
-    int x = board->cursor.x * width + BOARD_START.x;
-    int y = board->cursor.y * height + BOARD_START.y;
-    DrawRectangle(x, y, width, height, board->cursor.color);
+    int x = board.cursor.x * width + BOARD_START.x;
+    int y = board.cursor.y * height + BOARD_START.y;
+    DrawRectangle(x, y, width, height, board.cursor.color);
 
     // Include one more for bottom line drawing
-    for (int i = 0; i <= board->rows; i++) {
+    for (int i = 0; i <= board.rows; i++) {
         int y = BOARD_START.y + i * height;
         DrawLine(BOARD_START.x, y, BOARD_END.x, y, BLACK);
         int x = BOARD_START.x + i * width;
         DrawLine(x, BOARD_START.y, x, BOARD_END.y, BLACK);
     }
 
-    // renderBoardNumbers(board);
+    render_board_number(board);
     // renderBoardStatusBar(board.cursor.x + 1, board.cursor.y + 1);
 }
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "V-Draw");
-    Board board{10, 10};
+    Board board{40, 40};
     while (!WindowShouldClose()) {
         BeginDrawing();
-        render_board(&board);
+        render_board(board);
         EndDrawing();
     }
 
